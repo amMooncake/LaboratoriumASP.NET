277752296f4d586f0fde1a6f2f4784f5b393e2c9
alekm <alekmalawski123@gmail.com>
2024-10-09 12:06:07 +0200

    comit

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -28,8 +28,41 @@ public class HomeController : Controller
         return View();
     }
     
-    public IActionResult Calculator()
+    public IActionResult Calculator( string op, double? x, double? y)
     {
+        // var op = Request.Query["op"];
+        // var x = double.Parse(Request.Query["x"]);
+        // var y = double.Parse(Request.Query["y"]);
+
+        if (x == null || y == null)
+        {
+            ViewBag.ErrorMessage = "Please fill all values";
+            return View("CalculatorError");
+        }
+        
+        double? result = 0.0d;
+        switch(op)
+        {
+            case "add":
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case "sub":
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case "mul":
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case "div":
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+        }
+        ViewBag.result = result;
+        ViewBag.x = x;
+        ViewBag.y = y;
         return View();
     }
     
