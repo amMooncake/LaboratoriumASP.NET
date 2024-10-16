127318bb8b6d41802c270dab7904293bbf0cfe8e
alekm <alekmalawski123@gmail.com>
2024-10-16 11:20:24 +0200

    improvements

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Form()
+    {
+        return View();
+    }
+    
+    public IActionResult Result(string op, double? x, double? y)
+    {
+        //
+        // var op = Request.Query["op"];
+        // var x = double.Parse(Request.Query["x"]);
+        // var y = double.Parse(Request.Query["y"]);
+
+        // if (x == null || y == null)
+        // {
+        //     ViewBag.ErrorMessage = "Please fill all values";
+        //     return View("CalculatorError");
+        // }
+        //
+        // if (op is null)
+        // {
+        //     ViewBag.ErrorMessage = "Invalid operation";
+        //     return View("CalculatorError");
+        // }
+        //
+        
+        double? result = 0.0d;
+        switch(op)
+        {
+            case "+":
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case "-":
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case "*":
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case "/":
+                result = x / y;
+                ViewBag.Operator = ":";
+                break;
+        }
+        ViewBag.result = result;
+        ViewBag.x = x;
+        ViewBag.y = y;
+        return View();
+    }
+    
+}
+
